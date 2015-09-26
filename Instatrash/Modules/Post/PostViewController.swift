dc1467a53cf78ff53c5cb57e3633c8ff8f842cbb
Henrique Valcanaia <henrique.valcanaia@hotmail.com>
2015-09-26 05:26:51 -0300

    Comentario

--- Instatrash/Modules/Post/PostViewController.swift
+++ Instatrash/Modules/Post/PostViewController.swift
@@ -11,17 +11,6 @@ import Parse
 
 class PostViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     @IBAction func post(_: AnyObject) {
         let post = Post()
         post.user = PFUser.currentUser()
