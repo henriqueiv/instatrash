1285de0fa7fee2aa231f20c3d37976dacd8505bc
William Hass <wm.hass@gmail.com>
2015-09-26 04:23:58 -0300

    new view

---
+++ Instatrash/Modules/Posts/PostsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PostsViewController.swift
+//  Instatrash
+//
+//  Created by William Hass on 9/26/15.
+//  Copyright © 2015 Instatrash Inc. All rights reserved.
+//
+
+import UIKit
+
+class PostsViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var emptyStateView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+    
+}
